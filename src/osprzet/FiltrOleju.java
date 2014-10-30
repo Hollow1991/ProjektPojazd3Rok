b518f17b403fc303c288ba26d0a0520258907d13
sebek92 <sebastian.semczyszyn@gmail.com>
2014-10-30 11:17:40 +0100

    utworzono klase Antena w pakiecie nadwozie i Radio w pakiecie elktronika

---
+++ src/osprzet/FiltrOleju.java
@@ -0,0 +1,12 @@
+/**
+ * Klasa reprezentuje Autoalarm
+ * @author Patryk Struzik
+ */
+package osprzet;
+
+    /**
+     * Tworzy obiekt klasy FiltrOleju
+     */
+public class FiltrOleju {
+    
+}
