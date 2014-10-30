89056855786cdcb1fbb908b6997bae170bc0825b
student <student@91.218.164.149>
2014-10-30 11:11:11 +0100

    zmiana private.properties

--- src/pojazd/Pojazd.java
+++ src/pojazd/Pojazd.java
@@ -6,10 +6,28 @@
 
 package pojazd;
 
+import nadwozie.*;
 /**
  *
  * @author Krzysztof Poznakowski
  */
+
 public class Pojazd {
     
+    private Nadwozie nadwozie;
+    private Podwozie podwozie;
+    private Silnik silnik;
+    private Osprzet osprzet;
+    private Elektronika elektronika;
+    
+    public Pojazd() {
+        
+    nadwozie = new Nadwozie();
+    podwozie = new Podwozie();
+    silnik = new Silnik();
+    osprzet = new Osprzet();
+    eletronika = new Elektronika();
+    
+    System.out.println("[nadwozie.Lusterko] Utworzono obiekt klasy Lusterko");
+    }
 }
