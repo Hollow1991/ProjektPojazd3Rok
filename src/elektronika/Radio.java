fa1cca671e725f18335b92be74a2ea9aed47262a
sebek92 <sebastian.semczyszyn@gmail.com>
2014-10-30 11:06:09 +0100

    utworzono klase Antena w pakiecie nadwozie i Radio w pakiecie elktronika

---
+++ src/elektronika/Radio.java
@@ -0,0 +1,15 @@
+package elektronika;
+
+/**
+ * Klasa służy do reprezentowania Radia w samochodzie
+ * @author Sebastian
+ */
+public class Radio {
+    /**
+     * Tworzy obiekt klasy Radio
+     */
+    public Radio(){
+        
+        System.out.println("[elektronika.Radio] Utworzono obiekt klasy Radio");
+    }//public Radio
+}//public Class Radio
---
+++ src/nadwozie/Antena.java
@@ -0,0 +1,15 @@
+package nadwozie;
+
+/**
+ * Klasa służy do reprezentowania anteny w pojeździe
+ * @author Sebastian
+ */
+public class Antena {
+    /**
+     * Tworzy obiekt klasy Antena
+     */
+    public Antena(){
+        System.out.println("[nadwozie.Antena] Utworzono obiekt klasy Antena");
+        
+    }//public Antena
+}//public class Antena
