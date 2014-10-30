53852d15619cda0ce41232f159d9ccb12039b318
Patryk Struzik <patryk.struzik@gmail.com>
2014-10-30 11:16:19 +0100

    Poprawka

--- src/osprzet/FiltrOleju.java
+++ src/osprzet/FiltrOleju.java
@@ -8,6 +8,8 @@ package osprzet;
      * Tworzy obiekt klasy FiltrOleju
      */
 public class FiltrOleju {
+    public FiltrOleju()
+    {
      System.out.println("[nadwozie.Lusterko] Utworzono obiekt klasy FiltrOleju");
-    
+    }
 }
