b172d783f355ab56d74283b423f8ead0be987d03
Wadim Grudew <vadim_grudev@mail.ru>
2014-10-31 20:44:25 +0100

    Add new clasess: BlokSilnika, Wahacz

---
+++ src/podwozie/zawieszenie/Wahacz.java
@@ -0,0 +1,18 @@
+package podwozie.zawieszenie;
+
+/**
+ * Klasa służy do reprezentowania wahacza
+ *
+ * @author Wadim Grudew
+ */
+public class Wahacz {
+
+    //konstruktor
+    /**
+     * Tworzy obiekt klasy Wahacz
+     */
+    public Wahacz() {
+        System.out.println("[podwozie.zawieszenie.Wahacz] Utworzo obiekt klasy Wahacz");
+    }//public Wahacz()
+
+}//public class Wahacz
---
+++ src/silnik/BlokSilnika.java
@@ -0,0 +1,18 @@
+package silnik;
+
+/**
+ * Klasa służy do reprezentowania bloka silnika
+ *
+ * @author Wadim Grudew
+ */
+public class BlokSilnika {
+
+    //konstruktor
+    /**
+     * Tworzy obiekt klasy BlokSilnika
+     */
+    public BlokSilnika() {
+        System.out.println("[silnik.BlokSilnika] Utworzo obiekt klasy BlokSilnika");
+    } //public BlokSilnika()
+
+}//public class BlokSilnika
