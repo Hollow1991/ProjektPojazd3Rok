4b82f21cf39fbe4e04358fb4eb678938e08dbe18
Paulina Mela <paulinamela@gmail.com>
2014-11-06 10:18:35 +0100

    Dodano dwie klasy KolektorSsacy oraz GlowicaCylindrow

---
+++ src/silnik/GlowicaCylindrow.java
@@ -0,0 +1,16 @@
+package silnik;
+
+/**
+ * Służy do opisu glowicy cylindrow
+ * @author paulinamela
+ */
+public class GlowicaCylindrow {
+    
+    /**
+     * Tworzy obiekt klasy GlowicaCylindrow
+     */
+    
+    public GlowicaCylindrow() {
+        System.out.println("[silnik.GlowicaCylindrow] Utworzono obiekt klasy GlowicaCylindrow");
+    }//public GlowicaCylindrow
+}//public class GlowicaCylindrow
---
+++ src/silnik/KolektorSsacy.java
@@ -0,0 +1,16 @@
+package silnik;
+
+/**
+ * Służy do opisu kolektora ssacego
+ * @author paulinamela
+ */
+public class KolektorSsacy {
+    
+    /**
+     * Tworzy obiekt klasy KolektorSsacy
+     */
+    
+    public KolektorSsacy() {
+        System.out.println("[silnik.KolektorSsacy] Utworzono obiekt klasy KolektorSsacy");
+    }//public KolektorSsacy
+}//public class KolektorSsacy
