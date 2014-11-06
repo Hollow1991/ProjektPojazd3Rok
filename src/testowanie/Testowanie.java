f916a6c6a7f32f5d254cb811aa158704de4a9d3d
Dominik Szałkowski <dominisz@gmail.com>
2014-11-06 10:04:31 +0100

    Dodany plik Testowanie.java

---
+++ src/testowanie/Testowanie.java
@@ -0,0 +1,15 @@
+package testowanie;
+
+import podwozie.zawieszenie.Wahacz;
+
+/**
+ *
+ * @author dominisz
+ */
+public class Testowanie {
+    
+    public static void main(String[] args) {
+        Wahacz wahacz=new Wahacz();
+    }//public static void main
+    
+}//public class Testowanie
