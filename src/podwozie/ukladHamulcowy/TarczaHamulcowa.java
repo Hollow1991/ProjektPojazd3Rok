a7ca98429fdbb34fe077edf59f02c58c97039004
przemekch92 <przemyslaw.p.chudzik@gmail.com>
2014-11-10 14:40:08 +0100

    dodalem klasy kierownica i tarczaHamulcowa do repozytorium

---
+++ src/podwozie/ukladHamulcowy/TarczaHamulcowa.java
@@ -0,0 +1,17 @@
+package podwozie.ukladHamulcowy;
+
+/**
+ *
+ * @author Przemysław Chudzik
+ */
+public class TarczaHamulcowa {
+    
+    /**
+     * Tworzy obiekt klasy TarczaHamulcowa
+     */
+    
+    public TarczaHamulcowa() {
+        System.out.println("[podwozie.ukladHamulcowy.TarczaHamulcowa] Utworzono obiekt klasy TarczaHamulcowa");
+    }//public TarczaHamulcowa
+    
+}//public class TarczaHamulcowa
---
+++ src/podwozie/ukladKierowniczy/Kierownica.java
@@ -0,0 +1,16 @@
+package podwozie.ukladKierowniczy;
+
+/**
+ *
+ * @author Przemysław Chudzik
+ */
+public class Kierownica {
+    /**
+     * Tworze obiekt klasy Kierownica
+     */
+    public Kierownica()
+    {
+        System.out.println("[podwozie.ukladKierowniczy.Kierownica] Utworzono obiekt klasy UkladKierowniczy");
+    }//public Kierownica
+    
+}//public class Kierownica
