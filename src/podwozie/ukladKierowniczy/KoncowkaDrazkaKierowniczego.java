32a85588f04492038556d686c776ffc5f9228c0c
Aleksey Czerkaszyn <justabomberman@gmail.com>
2014-11-13 19:37:11 +0100

    Adding new classes

---
+++ src/podwozie/ukladKierowniczy/KoncowkaDrazkaKierowniczego.java
@@ -0,0 +1,17 @@
+package ukladKierowniczy;
+
+/**
+ *
+ * @author Aleksey Czerkaszyn
+ */
+public class KoncowkaDrazkaKierowniczego {
+ 
+     /**
+     * Tworzy obiekt klasy KoncowkaDrazkaKierowniczego
+     */
+    
+    public KoncowkaDrazkaKierowniczego() {
+        System.out.println("[ukladKierowniczy.KoncowkaDrazkaKierowniczego] Utworzo obiekt klasy KoncowkaDrazkaKierowniczego");
+}
+    
+}
---
+++ src/podwozie/ukladKierowniczy/Zwrotnica.java
@@ -0,0 +1,16 @@
+package ukladKierowniczy;
+
+/**
+ *
+ * @author Aleksey Czerkaszyn
+ */
+public class Zwrotnica {
+    
+    /**
+     * Tworzy obiekt klasy Zwrotnica
+     */
+    
+    public Zwrotnica() {
+        System.out.println("[ukladKierowniczy.Zwrotnica] Utworzono obiekt klasy Maska");
+    }
+}
