9359a6e72f734c134e1718c1101e6f13282210ec
radekkowalik90 <radoslaw.kowalik90@gmail.com>
2014-11-13 12:12:52 +0100

    Dodanie klas szyba i spryskiwacz w pakiecie nadwozie

---
+++ src/nadwozie/Spryskiwacz.java
@@ -0,0 +1,16 @@
+package nadwozie;
+
+/**
+ * Klasa służy do reprezentowania spryskiwaczy w pojeździe
+ * @author Radosław
+ */
+public class Spryskiwacz {
+    
+    /**
+     * Tworzy obiekt klasy Spryskiwacz
+     */
+    public Spryskiwacz() {
+        System.out.println("[nadwozie.Spryskiwacz] Utworzo obiekt klasy Spryskiwacz");
+    }//public Spryskiwacz
+    
+}//public class Spryskiwacz
---
+++ src/nadwozie/Szyba.java
@@ -0,0 +1,16 @@
+package nadwozie;
+
+/**
+ * Klasa służy do reprezentowania szyb w pojeździe
+ * @author Radosław
+ */
+public class Szyba {
+    
+    /**
+     * Tworzy obiekt klasy Szyba
+     */
+    public Szyba() {
+        System.out.println("[nadwozie.Szyba] Utworzo obiekt klasy Szyba");
+    }//public Szyba
+    
+}//public class Szyba
